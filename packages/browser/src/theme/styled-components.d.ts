02893cdecd11f62c577306b283d5440ba2057560
VitorSoer <contato.soer@gmail.com>
2024-07-29 14:42:25 -0300

    chore(browser): add styled-components types.

--- packages/browser/src/theme/styled-components.d.ts
+++ packages/browser/src/theme/styled-components.d.ts
@@ -1,7 +1,5 @@
 import 'styled-components';
-import { DefaultTheme } from 'styled-components';
 import { ThemeType } from '@shiba_ui/shared/src/theme';
-import { ColorKeys } from '@shiba_ui/shared/src/constants/color';
 
 declare module 'styled-components' {
   export interface DefaultTheme extends ThemeType {}
