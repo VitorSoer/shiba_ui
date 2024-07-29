0e2f292fa7be875183f163296aa2ed9c58488bce
VitorSoer <contato.soer@gmail.com>
2024-07-29 15:23:09 -0300

    chore(mobile): add styled-components configuration.

---
+++ packages/mobile/src/theme/index.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import theme from '@shiba_ui/shared/src/theme/';
+import { ThemeProvider, DefaultTheme } from 'styled-components/native';
+
+export const StyledThemeProvider: React.FC<{
+  children: React.ReactNode;
+  customTheme?: Partial<DefaultTheme>;
+}> = ({ children, customTheme = {} }) => {
+  const mergedTheme = { ...theme, ...customTheme };
+  return <ThemeProvider theme={mergedTheme}>{children}</ThemeProvider>;
+};
+
+export const withThemeProvider = <T extends object>(
+  Component: React.ComponentType<T>
+) => {
+  return (props: T) => (
+    <ThemeProvider theme={theme}>
+      <Component {...props} />
+    </ThemeProvider>
+  );
+};
