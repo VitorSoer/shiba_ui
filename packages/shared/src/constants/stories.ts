4621c86eee85f9f7de3883bb45b5c0f5f8e36f73
VitorSoer <contato.soer@gmail.com>
2024-08-04 13:02:02 -0300

    refactor(shared): adjust summarySizes constant.

--- packages/shared/src/constants/stories.ts
+++ packages/shared/src/constants/stories.ts
@@ -1,3 +1,3 @@
-export const summarySizes = '"xs" | "sm" | "md" | "lg"';
+export const summarySizes = '"rm" | "xs" | "sm" | "md" | "lg"';
 export const summaryColors =
   '"obsidian" | "charcoal" | "graphite" | "steel" | "silver" | "quartz" | "porcelain" | "cotton" | "violet" | "coral" | "emerald" | "aqua" | "sunflower" | "tangerine" | "crimson" | "azure"';
