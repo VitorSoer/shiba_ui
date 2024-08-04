42a6c3eccb23a75151982a8b6f172676645eace4
VitorSoer <contato.soer@gmail.com>
2024-08-04 13:35:36 -0300

    feat(mobile): add Spinner story.

--- packages/mobile/src/components/Spinner/Spinner.stories.tsx
+++ packages/mobile/src/components/Spinner/Spinner.stories.tsx
@@ -1,8 +1,23 @@
 import Spinner from '.';
+import { Meta, StoryObj } from '@storybook/react';
+import { ISpinner } from '@shiba_ui/shared/src/components/Spinner';
 
-export default {
-  title: 'Spinner',
+const meta: Meta<ISpinner> = {
+  title: 'Components/Spinner',
   component: Spinner,
+  argTypes: {
+    size: { control: { type: 'object' } },
+    color: { control: 'object' },
+  },
 };
 
-export const Default = {};
+export default meta;
+
+type Story = StoryObj<ISpinner>;
+
+export const Default: Story = {
+  args: {
+    size: 'md',
+    color: 'violet',
+  },
+};
